b027d2239880c33774b6113d716ba4a11e855712
thgengineering <maheshkumar.vishwakarma@ifi.tech>
2020-06-12 14:27:23 +0530

    This is the test

---
+++ asp_core_frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp_core_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
